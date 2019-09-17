97b98c8973738be7c0ab95533f174febaf415aee
aqche <39076898+aqche@users.noreply.github.com>
2019-09-17 11:48:17 -0500

    Frontend S3 encryption (#14)
    * add server side encryption and enforce ssl on frontend s3
    
    * remove enforce ssl policy from frontend bucket

--- terraform/frontend_bucket.tf
+++ terraform/frontend_bucket.tf
@@ -26,5 +26,13 @@ resource "aws_s3_bucket" "keyconjurer_frontend" {
 }
 POLICY
 
+    server_side_encryption_configuration {
+        rule {
+            apply_server_side_encryption_by_default {
+                sse_algorithm = "AES256"
+            }
+        }
+    }
+
     tags = "${var.tags}"
 }
