f9a11c4484966247b643214854876a9f44384efb
Dan <dan@aredherring.tech>
2021-01-29 14:58:07 +0000

    remove unneeded Execute() function

--- cli/main.go
+++ cli/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	err := Execute()
+	err := rootCmd.Execute()
 	if err == nil {
 		return
 	}
--- cli/root.go
+++ cli/root.go
@@ -147,9 +147,3 @@ func parseHostname(hostname string) (string, error) {
 func newClient() (Client, error) {
 	return NewClient(host)
 }
-
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
-func Execute() error {
-	return rootCmd.Execute()
-}
