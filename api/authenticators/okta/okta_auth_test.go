3f9208e26d2303a4f757309a84bd99a33c4e2a3a
Artem Smotrakov <asmotrakov@riotgames.com>
2022-01-10 11:01:29 +0000

    Added a test for error handling in oktaAuthClient.Authn()

--- api/authenticators/okta/okta_auth_test.go
+++ api/authenticators/okta/okta_auth_test.go
@@ -1,8 +1,12 @@
 package okta
 
 import (
+	"context"
 	"errors"
+	"io"
 	"net/http"
+	"net/url"
+	"strings"
 	"testing"
 
 	"github.com/okta/okta-sdk-golang/v2/okta"
@@ -33,4 +37,69 @@ func TestWrapError(t *testing.T) {
 	require.True(t, errors.Is(oktaErr, ErrOktaForbidden), "expected forbidden error")
 	require.Contains(t, oktaErr.Error(), ErrOktaForbidden.Error(), "expected forbidden text")
 	require.Contains(t, oktaErr.Error(), "access denied", "expected the original error text")
+}
+
+// RoundTripperMock implements RoundTripper interface for testing.
+type RoundTripperMock struct {
+	response *http.Response
+	err      error
+}
+
+// RoundTrip returns a pre-defined HTTP response and an error.
+func (o RoundTripperMock) RoundTrip(request *http.Request) (*http.Response, error) {
+	return o.response, o.err
+}
+
+var testOktaUrl url.URL = url.URL{}
+
+// MakeBody wraps a string into an io.ReadCloser.
+func MakeBody(s string) io.ReadCloser {
+	return io.NopCloser(strings.NewReader(s))
+}
+
+func TestAuthnWithOktaErrors(t *testing.T) {
+	httpTransportMock := RoundTripperMock{}
+	httpTransportMock.response = nil
+	httpTransportMock.err = errors.New("connection failed")
+	client := oktaAuthClient{url: testOktaUrl, rt: httpTransportMock}
+	_, oktaErr := client.Authn(context.TODO(), authnRequest{})
+	require.True(t, errors.Is(oktaErr, ErrOktaInternalServerError), "expected internal server error")
+	require.Contains(t, oktaErr.Error(), ErrOktaInternalServerError.Error(), "expected internal server error message")
+	require.Contains(t, oktaErr.Error(), httpTransportMock.err.Error(), "expected the original error message")
+
+	httpTransportMock = RoundTripperMock{}
+	oktaReply := `
+	{
+		"errorCode": "E0000004",
+		"errorSummary": "Authentication failed",
+		"errorLink": "E0000004",
+		"errorId": "sampleXhLJI0ZNxN0ab8IobVb",
+		"errorCauses": []
+	}
+	`
+	httpTransportMock.response = &http.Response{StatusCode: 401, Body: MakeBody(oktaReply)}
+	httpTransportMock.err = nil
+	client = oktaAuthClient{url: testOktaUrl, rt: httpTransportMock}
+	_, oktaErr = client.Authn(context.TODO(), authnRequest{})
+	require.True(t, errors.Is(oktaErr, ErrOktaUnauthorized), "expected unauthorized error")
+	require.Contains(t, oktaErr.Error(), ErrOktaUnauthorized.Error(), "expected unauthorized error message")
+	require.Contains(t, oktaErr.Error(), "Authentication failed", "expected error summary from the Okta reply")
+
+	httpTransportMock = RoundTripperMock{}
+	oktaReply = `
+	{
+		"errorCode": "E0000006",
+		"errorSummary": "You do not have permission to perform the requested action",
+		"errorLink": "E0000006",
+		"errorId": "sampleBmGsRUZa0_Nsv82RoOL",
+		"errorCauses": []
+	}
+	`
+	httpTransportMock.response = &http.Response{StatusCode: 403, Body: MakeBody(oktaReply)}
+	httpTransportMock.err = nil
+	client = oktaAuthClient{url: testOktaUrl, rt: httpTransportMock}
+	_, oktaErr = client.Authn(context.TODO(), authnRequest{})
+	require.True(t, errors.Is(oktaErr, ErrOktaForbidden), "expected forbidden error")
+	require.Contains(t, oktaErr.Error(), ErrOktaForbidden.Error(), "expected forbidden message")
+	require.Contains(t, oktaErr.Error(), "You do not have permission to perform the requested action", "expected error summary from the Okta reply")
 }
