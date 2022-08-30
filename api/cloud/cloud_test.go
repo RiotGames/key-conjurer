94e9a41280fd717b03096742ebb7df2c255ac9ab
Dan Pantry <dpantry@riotgames.com>
2022-08-30 15:52:08 -0700

    Resolve test failure

--- api/cloud/cloud_test.go
+++ api/cloud/cloud_test.go
@@ -9,8 +9,8 @@ import (
 
 func TestAwsGetRoleDoesntBreakIfYouHaveMultipleRoles(t *testing.T) {
 	resp := saml.Response{}
-	resp.AddAttribute("https://cloud.amazon.com/SAML/Attributes/Role", "arn:cloud:iam::1234:saml-provider/Okta,arn:cloud:iam::1234:role/Admin")
-	resp.AddAttribute("https://cloud.amazon.com/SAML/Attributes/Role", "arn:cloud:iam::1234:saml-provider/Okta,arn:cloud:iam::1234:role/Power")
+	resp.AddAttribute("https://aws.amazon.com/SAML/Attributes/Role", "arn:cloud:iam::1234:saml-provider/Okta,arn:cloud:iam::1234:role/Admin")
+	resp.AddAttribute("https://aws.amazon.com/SAML/Attributes/Role", "arn:cloud:iam::1234:saml-provider/Okta,arn:cloud:iam::1234:role/Power")
 	providerARN, roleARN, _, err := getRole("Power", &resp)
 	require.NoError(t, err)
 	require.Equal(t, "arn:cloud:iam::1234:saml-provider/Okta", providerARN)
@@ -24,7 +24,7 @@ func TestAwsGetRoleDoesntBreakIfYouHaveMultipleRoles(t *testing.T) {
 func TestAwsGetRoleWorksWithOneLoginAssertions(t *testing.T) {
 	resp := saml.Response{}
 	// For some reason, this is reversed in OneLogin.
-	resp.AddAttribute("https://cloud.amazon.com/SAML/Attributes/Role", "arn:cloud:iam::1234:role/Admin,arn:cloud:iam::1234:saml-provider/Onelogin")
+	resp.AddAttribute("https://aws.amazon.com/SAML/Attributes/Role", "arn:cloud:iam::1234:role/Admin,arn:cloud:iam::1234:saml-provider/Onelogin")
 	providerARN, roleARN, _, err := getRole("", &resp)
 	require.NoError(t, err)
 	require.Equal(t, "arn:cloud:iam::1234:saml-provider/Onelogin", providerARN)
