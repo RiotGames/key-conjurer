11ffaf6c57d4c59c77b919eba650f4036343faec
Dan Pantry <dpantry@riotgames.com>
2023-01-25 14:41:57 -0800

    Remove inline policy

--- terraform/frontend_bucket.tf
+++ terraform/frontend_bucket.tf
@@ -1,36 +1,45 @@
 resource "aws_s3_bucket" "keyconjurer_frontend" {
   bucket = "keyconjurer-frontend-${terraform.workspace}"
-  acl    = "private"
-  policy = <<POLICY
-{
-    "Version": "2012-10-17",
-    "Id": "KeyConjurerAccess",
-    "Statement": [
-        {
-            "Sid": "Grant a CloudFront Origin Identity access",
-            "Effect": "Allow",
-            "Principal": {
-                "CanonicalUser": "${aws_cloudfront_origin_access_identity.keyconjurer_identity.s3_canonical_user_id}"
-            },
-            "Action": "s3:GetObject",
-            "Resource": "arn:aws:s3:::keyconjurer-frontend-${terraform.workspace}/*"
-        },
-        {
-            "Sid": "CI Upload",
-            "Effect": "Allow",
-            "Principal": { "AWS": "arn:aws:iam::${var.account_number}:role/infosec_ci" },
-            "Action": "s3:PutObject",
-            "Resource": "arn:aws:s3:::keyconjurer-frontend-${terraform.workspace}/*"
-        }
-    ]
 }
-POLICY
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+data "aws_iam_policy_document" "frontend_bucket_policy" {
+  statement {
+    sid       = "Cloudfront Access"
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.keyconjurer_frontend.arn}/*"]
+    principals {
+      type        = "CanonicalUser"
+      identifiers = [aws_cloudfront_origin_access_identity.keyconjurer_identity.s3_canonical_user_id]
+    }
+  }
+
+  statement {
+    sid       = "CI Upload"
+    actions   = ["s3:PutObject"]
+    resources = ["${aws_s3_bucket.keyconjurer_frontend.arn}/*"]
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${var.account_number}:role/infosec_ci"]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "frontend_bucket" {
+  bucket = aws_s3_bucket.keyconjurer_frontend.bucket
+  policy = data.aws_iam_policy_document.frontend_bucket_policy.json
+}
+
+
+resource "aws_s3_bucket_acl" "frontend_bucket" {
+  bucket = aws_s3_bucket.keyconjurer_frontend.bucket
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "frontend_bucket" {
+  bucket = aws_s3_bucket.keyconjurer_frontend.bucket
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
     }
   }
 }
