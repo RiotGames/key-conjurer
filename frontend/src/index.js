aa28a3ea6dd43eb12a2444782bed70d684467300
Dan Pantry <dpantry@riotgames.com>
2023-04-15 20:37:23 -0700

    Remove service worker

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { unregister } from "./registerServiceWorker";
 import "./index.css";
 import "semantic-ui-css/semantic.min.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-unregister();
