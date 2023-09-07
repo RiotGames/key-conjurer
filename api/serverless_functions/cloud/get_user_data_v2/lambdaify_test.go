97c7beb9a67f64e1ae62cff696e34b07af259788
Dan Pantry <dpantry@riotgames.com>
2023-09-07 10:04:20 -0700

    add lambda <-> net/http translation layer

--- api/serverless_functions/cloud/get_user_data_v2/lambdaify_test.go
+++ api/serverless_functions/cloud/get_user_data_v2/lambdaify_test.go
@@ -1,11 +1,46 @@
 package main
 
-import "testing"
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"net/url"
+	"testing"
 
-func TestLambdaify_ConvertsALBRequestIntoHttpRequest(t *testing.T) {
-	t.Skipf("Not yet implemented")
-}
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
 
-func TestLambdaify_ConvertsHttpResponseIntoALBResponse(t *testing.T) {
-	t.Skipf("Not yet implemented")
+func TestLambdaify_ALBTargetEvents(t *testing.T) {
+	vals := url.Values{
+		"id_token": {"id token goes here"},
+	}
+
+	inboundEvent := events.ALBTargetGroupRequest{
+		HTTPMethod: "POST",
+		Path:       "/hello-world",
+		Headers: map[string]string{
+			"Content-Type": "application/x-www-form-urlencoded",
+		},
+		Body: vals.Encode(),
+	}
+
+	inboundEventBytes, err := json.Marshal(inboundEvent)
+	require.NoError(t, err, "Could not marshal inbound event to JSON")
+
+	handler := lambdaify(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, r.Method, "POST")
+		assert.Equal(t, r.URL.Path, "/hello-world")
+		assert.Equal(t, r.FormValue("id_token"), "id token goes here")
+
+		w.Write([]byte("Hello, world!"))
+	}))
+
+	payload, err := handler.Invoke(context.Background(), []byte(inboundEventBytes))
+	require.NoError(t, err, "Could not invoke Lambda handler")
+
+	var resp events.ALBTargetGroupResponse
+	require.NoError(t, json.Unmarshal(payload, &resp), "Could not unmarshal JSON")
+	assert.Equal(t, resp.Body, "Hello, world!")
 }
--- api/serverless_functions/cloud/get_user_data_v2/main.go
+++ api/serverless_functions/cloud/get_user_data_v2/main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"io"
 	"net/http"
+	"net/url"
+	"strings"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/okta/okta-sdk-golang/v2/okta"
 )
@@ -13,8 +19,62 @@ func ServeUserApplications(client *okta.Client) http.Handler {
 	})
 }
 
+type lambdaResponseWriter struct {
+	TargetGroupResponse events.ALBTargetGroupResponse
+}
+
+func (w *lambdaResponseWriter) Header() http.Header {
+	if w.TargetGroupResponse.MultiValueHeaders == nil {
+		w.TargetGroupResponse.MultiValueHeaders = make(map[string][]string)
+	}
+
+	return http.Header(w.TargetGroupResponse.MultiValueHeaders)
+}
+
+func (w *lambdaResponseWriter) WriteHeader(code int) {
+	w.TargetGroupResponse.StatusCode = code
+}
+
+func (w *lambdaResponseWriter) Write(b []byte) (int, error) {
+	w.TargetGroupResponse.Body = string(b)
+	return len(b), nil
+}
+
+type lambda2HttpHandler struct {
+	next http.Handler
+}
+
+func (h lambda2HttpHandler) Invoke(ctx context.Context, b []byte) ([]byte, error) {
+	var inboundReq events.ALBTargetGroupRequest
+	if err := json.Unmarshal(b, &inboundReq); err != nil {
+		return nil, err
+	}
+
+	header := http.Header{}
+	if len(inboundReq.MultiValueHeaders) > 0 {
+		header = http.Header(inboundReq.MultiValueHeaders)
+	} else {
+		for k, v := range inboundReq.Headers {
+			header[k] = []string{v}
+		}
+	}
+
+	req := http.Request{
+		Method: inboundReq.HTTPMethod,
+		URL: &url.URL{
+			Path: inboundReq.Path,
+		},
+		Header: header,
+		Body:   io.NopCloser(strings.NewReader(inboundReq.Body)),
+	}
+
+	var respWriter lambdaResponseWriter
+	h.next.ServeHTTP(&respWriter, &req)
+	return json.Marshal(respWriter.TargetGroupResponse)
+}
+
 func lambdaify(next http.Handler) lambda.Handler {
-	return nil
+	return lambda2HttpHandler{next}
 }
 
 func main() {
