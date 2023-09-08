fe124d8d8ed365448d420ec55da36f85fc98c59f
Dan Pantry <dpantry@riotgames.com>
2023-09-08 10:58:37 -0700

    Provide secrets to the new Lambda function

--- api/serverless_functions/list_applications_v2/main.go
+++ api/serverless_functions/list_applications_v2/main.go
@@ -1,13 +1,27 @@
 package main
 
 import (
+	"log"
+	"net/url"
+
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/riotgames/key-conjurer/api/keyconjurer"
+	"github.com/riotgames/key-conjurer/api/settings"
 	"github.com/riotgames/key-conjurer/pkg/httputil"
 )
 
 func main() {
-	var service keyconjurer.OktaService
+	settings, err := settings.NewSettings()
+	if err != nil {
+		log.Fatalf("Could not fetch configuration: %s", err)
+	}
+
+	oktaDomain := url.URL{
+		Scheme: "https",
+		Host:   settings.OktaHost,
+	}
+
+	service := keyconjurer.NewOktaService(&oktaDomain, settings.OktaToken)
 	fn := httputil.Lambdaify(keyconjurer.ServeUserApplications(service))
 	lambda.Start(fn)
 }
