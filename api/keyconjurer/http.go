177f761e1f4f3744c503f2f66403b64a2bfa1769
Dan <2939173+trinitroglycerin@users.noreply.github.com>
2023-09-09 11:46:47 -0700

    Move serverless function to the serverless functions file

--- api/keyconjurer/http.go
+++ api/keyconjurer/http.go
@@ -1,14 +1,8 @@
 package keyconjurer
 
 import (
-	"context"
 	"net/http"
 	"strings"
-
-	"github.com/okta/okta-sdk-golang/v2/okta"
-	"github.com/riotgames/key-conjurer/api/core"
-	"github.com/riotgames/key-conjurer/pkg/httputil"
-	"golang.org/x/exp/slog"
 )
 
 func GetBearerToken(r *http.Request) (string, bool) {
@@ -27,63 +21,4 @@ func GetBearerToken(r *http.Request) (string, bool) {
 	}
 
 	return parts[1], parts[0] == "Bearer"
-}
-
-type OktaService interface {
-	GetUserInfo(ctx context.Context, token string) (OktaUserInfo, error)
-	ListApplicationsForUser(ctx context.Context, user string) ([]*okta.AppLink, error)
-}
-
-func ServeUserApplications(okta OktaService) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		attrs := []any{
-			slog.String("origin_ip_address", r.RemoteAddr),
-		}
-
-		if v, ok := r.Header["X-Amzn-Trace-Id"]; ok {
-			attrs = append(attrs, slog.String("amz_request_id", v[0]))
-		}
-
-		if v, ok := r.Header["X-Forwarded-For"]; ok {
-			attrs = append(attrs, slog.String("x_forwarded_for", v[0]))
-		}
-
-		idToken, ok := GetBearerToken(r)
-		if !ok {
-			slog.Error("no bearer token present", attrs...)
-			httputil.ServeJSONError(w, http.StatusUnauthorized, "unauthorized")
-			return
-		}
-
-		info, err := okta.GetUserInfo(r.Context(), idToken)
-		if err != nil {
-			attrs = append(attrs, slog.String("error", err.Error()))
-			slog.Error("okta rejected id token", attrs...)
-			httputil.ServeJSONError(w, http.StatusForbidden, "unauthorized")
-			return
-		}
-
-		attrs = append(attrs, slog.String("username", info.PreferredUsername))
-		applications, err := okta.ListApplicationsForUser(r.Context(), info.PreferredUsername)
-		if err != nil {
-			attrs = append(attrs, slog.String("error", err.Error()))
-			slog.Error("failed to fetch applications", attrs...)
-			httputil.ServeJSONError(w, http.StatusBadGateway, "upstream error")
-			return
-		}
-
-		var accounts []core.Application
-		for i, app := range applications {
-			if app.AppName == "amazon_aws" || strings.Contains(app.AppName, "tencent") {
-				accounts[i] = core.Application{
-					ID:   app.Id,
-					Name: app.Label,
-				}
-			}
-		}
-
-		attrs = append(attrs, slog.Int("application_count", len(accounts)))
-		slog.Info("served applications", attrs...)
-		httputil.ServeJSON(w, accounts)
-	})
 }
--- api/keyconjurer/serverless_functions.go
+++ api/keyconjurer/serverless_functions.go
@@ -5,16 +5,21 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"net/http"
+	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/okta/okta-sdk-golang/v2/okta"
 	"github.com/riotgames/key-conjurer/api/consts"
 	"github.com/riotgames/key-conjurer/api/core"
 	"github.com/riotgames/key-conjurer/api/settings"
 	"github.com/riotgames/key-conjurer/internal"
+	"github.com/riotgames/key-conjurer/pkg/httputil"
 	"github.com/riotgames/key-conjurer/providers"
-	"github.com/riotgames/key-conjurer/providers/okta"
+	kcokta "github.com/riotgames/key-conjurer/providers/okta"
 	"github.com/sirupsen/logrus"
+	"golang.org/x/exp/slog"
 )
 
 type Handler struct {
@@ -38,7 +43,7 @@ func NewHandler(cfg *settings.Settings) Handler {
 		})
 	}
 
-	ok, err := okta.New(cfg.OktaHost, cfg.OktaToken)
+	ok, err := kcokta.New(cfg.OktaHost, cfg.OktaToken)
 	if err != nil {
 		panic(err)
 	}
@@ -276,4 +281,63 @@ func getErrorCode(err error) ErrorCode {
 	default:
 		return ErrCodeUnspecified
 	}
+}
+
+type OktaService interface {
+	GetUserInfo(ctx context.Context, token string) (OktaUserInfo, error)
+	ListApplicationsForUser(ctx context.Context, user string) ([]*okta.AppLink, error)
+}
+
+func ServeUserApplications(okta OktaService) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		attrs := []any{
+			slog.String("origin_ip_address", r.RemoteAddr),
+		}
+
+		if v, ok := r.Header["X-Amzn-Trace-Id"]; ok {
+			attrs = append(attrs, slog.String("amz_request_id", v[0]))
+		}
+
+		if v, ok := r.Header["X-Forwarded-For"]; ok {
+			attrs = append(attrs, slog.String("x_forwarded_for", v[0]))
+		}
+
+		idToken, ok := GetBearerToken(r)
+		if !ok {
+			slog.Error("no bearer token present", attrs...)
+			httputil.ServeJSONError(w, http.StatusUnauthorized, "unauthorized")
+			return
+		}
+
+		info, err := okta.GetUserInfo(r.Context(), idToken)
+		if err != nil {
+			attrs = append(attrs, slog.String("error", err.Error()))
+			slog.Error("okta rejected id token", attrs...)
+			httputil.ServeJSONError(w, http.StatusForbidden, "unauthorized")
+			return
+		}
+
+		attrs = append(attrs, slog.String("username", info.PreferredUsername))
+		applications, err := okta.ListApplicationsForUser(r.Context(), info.PreferredUsername)
+		if err != nil {
+			attrs = append(attrs, slog.String("error", err.Error()))
+			slog.Error("failed to fetch applications", attrs...)
+			httputil.ServeJSONError(w, http.StatusBadGateway, "upstream error")
+			return
+		}
+
+		var accounts []core.Application
+		for i, app := range applications {
+			if app.AppName == "amazon_aws" || strings.Contains(app.AppName, "tencent") {
+				accounts[i] = core.Application{
+					ID:   app.Id,
+					Name: app.Label,
+				}
+			}
+		}
+
+		attrs = append(attrs, slog.Int("application_count", len(accounts)))
+		slog.Info("served applications", attrs...)
+		httputil.ServeJSON(w, accounts)
+	})
 }
