b97ed75e9c8b98217c15d54e0ad73fe6c99d70f5
Dan <2939173+trinitroglycerin@users.noreply.github.com>
2023-09-12 16:31:20 -0700

    Write errors in ServeJSON to log

--- pkg/httputil/json.go
+++ pkg/httputil/json.go
@@ -3,18 +3,22 @@ package httputil
 import (
 	"encoding/json"
 	"net/http"
+
+	"golang.org/x/exp/slog"
 )
 
 func ServeJSON[T any](w http.ResponseWriter, data T) {
 	buf, err := json.Marshal(data)
 	if err != nil {
 		// Nothing to be done here
+		slog.Error("could not marshal JSON: %s", err)
 		return
 	}
 
 	// Nothing we can do to respond to the error message here either
 	w.Header().Set("Content-Type", "application/json")
-	w.Write(buf)
+	_, err = w.Write(buf)
+	slog.Error("could not write JSON to the client: %s", err)
 }
 
 type JSONError struct {
