6745a6f8f50f3e8cafe3926b57f5d196efea992a
Dan <2939173+trinitroglycerin@users.noreply.github.com>
2023-09-17 12:20:12 -0700

    Remove the legacy API
    The legacy API can be deployed using Terraform and git subtrees. The
    mainline branch of the repository should not have any particular
    dependency on the deployment of the product

--- terraform/loadbalancer.tf
+++ terraform/loadbalancer.tf
@@ -40,51 +40,6 @@ resource "aws_lb_listener" "https_redirect" {
   }
 }
 
-resource "aws_lb_listener_rule" "get_aws_creds" {
-  listener_arn = aws_lb_listener.https.arn
-
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.get_aws_creds.arn
-  }
-
-  condition {
-    path_pattern {
-      values = ["/get_aws_creds"]
-    }
-  }
-}
-
-resource "aws_lb_listener_rule" "list_providers" {
-  listener_arn = aws_lb_listener.https.arn
-
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.list_providers.arn
-  }
-
-  condition {
-    path_pattern {
-      values = ["/list_providers"]
-    }
-  }
-}
-
-resource "aws_lb_listener_rule" "get_user_data" {
-  listener_arn = aws_lb_listener.https.arn
-
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.get_user_data.arn
-  }
-
-  condition {
-    path_pattern {
-      values = ["/get_user_data"]
-    }
-  }
-}
-
 resource "aws_lb_listener_rule" "list_applications_v2" {
   listener_arn = aws_lb_listener.https.arn
 
@@ -98,60 +53,6 @@ resource "aws_lb_listener_rule" "list_applications_v2" {
       values = ["/v2/applications"]
     }
   }
-}
-
-resource "aws_lb_target_group" "get_aws_creds" {
-  name_prefix = "keycon"
-  target_type = "lambda"
-}
-
-resource "aws_lb_target_group_attachment" "get_aws_creds" {
-  target_group_arn = aws_lb_target_group.get_aws_creds.arn
-  target_id        = aws_lambda_function.keyconjurer-get_aws_creds.arn
-}
-
-resource "aws_lambda_permission" "lb-get_aws_creds" {
-  statement_id  = "LoadBalancer"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.keyconjurer-get_aws_creds.arn
-  principal     = "elasticloadbalancing.amazonaws.com"
-  source_arn    = aws_lb_target_group.get_aws_creds.arn
-}
-
-resource "aws_lb_target_group" "get_user_data" {
-  name_prefix = "keycon"
-  target_type = "lambda"
-}
-
-resource "aws_lb_target_group_attachment" "get_user_data" {
-  target_group_arn = aws_lb_target_group.get_user_data.arn
-  target_id        = aws_lambda_function.keyconjurer-get_user_data.arn
-}
-
-resource "aws_lambda_permission" "lb-get_user_data" {
-  statement_id  = "LoadBalancer"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.keyconjurer-get_user_data.arn
-  principal     = "elasticloadbalancing.amazonaws.com"
-  source_arn    = aws_lb_target_group.get_user_data.arn
-}
-
-resource "aws_lb_target_group" "list_providers" {
-  name_prefix = "keycon"
-  target_type = "lambda"
-}
-
-resource "aws_lb_target_group_attachment" "list_providers" {
-  target_group_arn = aws_lb_target_group.list_providers.arn
-  target_id        = aws_lambda_function.keyconjurer-list_providers.arn
-}
-
-resource "aws_lambda_permission" "lb-list_providers" {
-  statement_id  = "LoadBalancer"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.keyconjurer-list_providers.arn
-  principal     = "elasticloadbalancing.amazonaws.com"
-  source_arn    = aws_lb_target_group.list_providers.arn
 }
 
 resource "aws_lb_target_group" "list_applications_v2" {
