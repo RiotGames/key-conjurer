1277d79bd1ad6f1c8fcec50189a6b82e99b603e7
Dan Pantry <dpantry@riotgames.com>
2024-11-11 16:19:29 -0800

    Correct key-value

--- internal/api/json.go
+++ internal/api/json.go
@@ -11,14 +11,14 @@ func ServeJSON[T any](w http.ResponseWriter, data T) {
 	buf, err := json.Marshal(data)
 	if err != nil {
 		// Nothing to be done here
-		slog.Error("could not marshal JSON: %s", err)
+		slog.Error("could not marshal JSON: %s", "error", err)
 		return
 	}
 
 	// Nothing we can do to respond to the error message here either
 	w.Header().Set("Content-Type", "application/json")
 	_, err = w.Write(buf)
-	slog.Error("could not write JSON to the client: %s", err)
+	slog.Error("could not write JSON to the client: %s", "error", err)
 }
 
 type JSONError struct {
