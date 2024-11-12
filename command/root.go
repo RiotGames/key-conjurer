af836d4fcb37c720378c83f041516551e7c05de9
Dan Pantry <dpantry@riotgames.com>
2024-11-12 11:09:24 -0800

    Add Config parsing

--- command/root.go
+++ command/root.go
@@ -6,7 +6,6 @@ import (
 	"os"
 	"path/filepath"
 	"runtime"
-	"time"
 
 	"github.com/alecthomas/kong"
 	"github.com/mitchellh/go-homedir"
@@ -18,15 +17,9 @@ var (
 	FlagClientID   = "client-id"
 	FlagConfigPath = "config"
 	FlagQuiet      = "quiet"
-	FlagTimeout    = "timeout"
 )
 
 func init() {
-	rootCmd.PersistentFlags().String(FlagOIDCDomain, OIDCDomain, "The domain name of your OIDC server")
-	rootCmd.PersistentFlags().String(FlagClientID, ClientID, "The OAuth2 Client ID for the application registered with your OIDC server")
-	rootCmd.PersistentFlags().Int(FlagTimeout, 120, "the amount of time in seconds to wait for keyconjurer to respond")
-	rootCmd.PersistentFlags().String(FlagConfigPath, "~/.keyconjurerrc", "path to .keyconjurerrc file")
-	rootCmd.PersistentFlags().Bool(FlagQuiet, false, "tells the CLI to be quiet; stdout will not contain human-readable informational messages")
 	rootCmd.AddCommand(accountsCmd)
 	rootCmd.AddCommand(getCmd)
 	rootCmd.AddCommand(setCmd)
@@ -35,9 +28,6 @@ func init() {
 	rootCmd.AddCommand(&unaliasCmd)
 	rootCmd.AddCommand(&rolesCmd)
 	rootCmd.SetVersionTemplate("{{.Version}}\n")
-
-	rootCmd.PersistentFlags().MarkHidden(FlagOIDCDomain)
-	rootCmd.PersistentFlags().MarkHidden(FlagClientID)
 }
 
 // rootCmd represents the base command when called without any subcommands
@@ -45,61 +35,8 @@ var rootCmd = &cobra.Command{
 	Use:     "keyconjurer",
 	Version: fmt.Sprintf("keyconjurer-%s-%s %s (%s)", runtime.GOOS, runtime.GOARCH, Version, BuildTimestamp),
 	Short:   "Retrieve temporary cloud credentials.",
-	Long: `KeyConjurer retrieves temporary credentials from Okta with the assistance of an optional API.
-
-To get started run the following commands:
-  keyconjurer login
-  keyconjurer accounts
-  keyconjurer get <accountName>
-`,
 	FParseErrWhitelist: cobra.FParseErrWhitelist{
 		UnknownFlags: true,
-	},
-	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
-		var config Config
-		// The error of this function call is only non-nil if the flag was not provided or is not a string.
-		configPath, _ := cmd.Flags().GetString(FlagConfigPath)
-		if expanded, err := homedir.Expand(configPath); err == nil {
-			configPath = expanded
-		}
-
-		file, err := EnsureConfigFileExists(configPath)
-		if err != nil {
-			return err
-		}
-
-		if err := config.Read(file); err != nil {
-			return err
-		}
-
-		// We don't care about this being cancelled.
-		timeout, _ := cmd.Flags().GetInt(FlagTimeout)
-		nextCtx, _ := context.WithTimeout(cmd.Context(), time.Duration(timeout)*time.Second)
-		cmd.SetContext(ConfigContext(nextCtx, &config, configPath))
-		return nil
-	},
-	PersistentPostRunE: func(cmd *cobra.Command, _ []string) error {
-		config := ConfigFromCommand(cmd)
-		path := ConfigPathFromCommand(cmd)
-		if expanded, err := homedir.Expand(path); err == nil {
-			path = expanded
-		}
-
-		// Do not use EnsureConfigFileExists here!
-		// EnsureConfigFileExists opens the file in append mode.
-		// If we open the file in append mode, we'll always append to the file. If we open the file in truncate mode before reading from the file, the content will be truncated _before we read from it_, which will cause a users configuration to be discarded every time we run the program.
-
-		if err := os.MkdirAll(filepath.Dir(path), os.ModeDir|os.FileMode(0755)); err != nil {
-			return err
-		}
-
-		file, err := os.Create(path)
-		if err != nil {
-			return fmt.Errorf("unable to create %s reason: %w", path, err)
-		}
-
-		defer file.Close()
-		return config.Write(file)
 	},
 	SilenceErrors: true,
 	SilenceUsage:  true,
@@ -109,6 +46,60 @@ type CLI struct {
 	Login LoginCommand `cmd:"" help:"Authenticate with KeyConjurer."`
 	Get   GetCommand   `cmd:"" help:"Retrieve temporary cloud credentials."`
 	// Switch SwitchCommand `cmd:"" help:"Switch between accounts."`
+
+	Config     Config `kong:"-"`
+	ConfigPath string `help:"path to .keyconjurerrc file" default:"~/.keyconjurerrc"`
+	Quiet      bool   `help:"tells the CLI to be quiet; stdout will not contain human-readable informational messages"`
+}
+
+func (CLI) Help() string {
+	return `KeyConjurer retrieves temporary credentials from Okta with the assistance of an optional API.
+
+To get started run the following commands:
+  keyconjurer login
+  keyconjurer accounts
+  keyconjurer get <accountName>`
+}
+
+func (c *CLI) BeforeApply(ctx *kong.Context, trace *kong.Path) error {
+	if expanded, err := homedir.Expand(c.ConfigPath); err == nil {
+		c.ConfigPath = expanded
+	}
+
+	file, err := EnsureConfigFileExists(c.ConfigPath)
+	if err != nil {
+		return err
+	}
+
+	err = c.Config.Read(file)
+	if err != nil {
+		return err
+	}
+
+	// Make *Config available to all sub-commands.
+	// This must be &c.Config because c.Config is not a pointer.
+	ctx.Bind(&c.Config)
+	return nil
+}
+
+func (c *CLI) AfterRun(ctx *kong.Context) error {
+	if expanded, err := homedir.Expand(c.ConfigPath); err == nil {
+		c.ConfigPath = expanded
+	}
+
+	// Do not use EnsureConfigFileExists here! EnsureConfigFileExists opens the file in append mode.
+	// If we open the file in append mode, we'll always append to the file. If we open the file in truncate mode before reading from the file, the content will be truncated _before we read from it_, which will cause a users configuration to be discarded every time we run the program.
+	if err := os.MkdirAll(filepath.Dir(c.ConfigPath), os.ModeDir|os.FileMode(0755)); err != nil {
+		return err
+	}
+
+	file, err := os.Create(c.ConfigPath)
+	if err != nil {
+		return fmt.Errorf("unable to create %s reason: %w", c.ConfigPath, err)
+	}
+
+	defer file.Close()
+	return c.Config.Write(file)
 }
 
 func Execute(ctx context.Context, args []string) error {
@@ -123,9 +114,5 @@ func Execute(ctx context.Context, args []string) error {
 		return err
 	}
 
-	var config Config
-	return kongCtx.Run(
-		// This should be moved to a hook in Root so that we can use flags to load the config.
-		&config,
-	)
+	return kongCtx.Run()
 }
