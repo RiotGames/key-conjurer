8c5e32e1d6523a9d2b21c808c8391c39e8d54817
Dan Pantry <dpantry@riotgames.com>
2025-02-07 14:51:57 -0800

    Save configuration to the config dir of the user, not the home dir

--- command/root.go
+++ command/root.go
@@ -4,20 +4,16 @@ import (
 	"context"
 	"fmt"
 	"net/http"
-	"os"
-	"path/filepath"
 	"runtime"
 	"time"
 
 	"github.com/coreos/go-oidc"
-	"github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
 )
 
 var (
 	FlagOIDCDomain = "oidc-domain"
 	FlagClientID   = "client-id"
-	FlagConfigPath = "config"
 	FlagQuiet      = "quiet"
 	FlagTimeout    = "timeout"
 )
@@ -26,7 +22,6 @@ func init() {
 	rootCmd.PersistentFlags().String(FlagOIDCDomain, OIDCDomain, "The domain name of your OIDC server")
 	rootCmd.PersistentFlags().String(FlagClientID, ClientID, "The OAuth2 Client ID for the application registered with your OIDC server")
 	rootCmd.PersistentFlags().Int(FlagTimeout, 120, "the amount of time in seconds to wait for keyconjurer to respond")
-	rootCmd.PersistentFlags().String(FlagConfigPath, "~/.keyconjurerrc", "path to .keyconjurerrc file")
 	rootCmd.PersistentFlags().Bool(FlagQuiet, false, "tells the CLI to be quiet; stdout will not contain human-readable informational messages")
 	rootCmd.AddCommand(loginCmd)
 	rootCmd.AddCommand(accountsCmd)
@@ -36,6 +31,17 @@ func init() {
 	rootCmd.AddCommand(&aliasCmd)
 	rootCmd.AddCommand(&unaliasCmd)
 	rootCmd.AddCommand(&rolesCmd)
+	rootCmd.AddCommand(&cobra.Command{
+		Use: "config-path",
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			path, err := findConfigPath()
+			if err != nil {
+				return err
+			}
+			fmt.Fprintln(cmd.OutOrStdout(), path)
+			return nil
+		},
+	})
 	rootCmd.SetVersionTemplate("{{.Version}}\n")
 
 	rootCmd.PersistentFlags().MarkHidden(FlagOIDCDomain)
@@ -58,50 +64,23 @@ To get started run the following commands:
 		UnknownFlags: true,
 	},
 	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
-		var config Config
-		// The error of this function call is only non-nil if the flag was not provided or is not a string.
-		configPath, _ := cmd.Flags().GetString(FlagConfigPath)
-		if expanded, err := homedir.Expand(configPath); err == nil {
-			configPath = expanded
-		}
-
-		file, err := EnsureConfigFileExists(configPath)
+		config, err := loadConfig()
 		if err != nil {
-			return err
-		}
-
-		if err := config.Read(file); err != nil {
-			return err
+			return fmt.Errorf("failed to load config: %s", err)
 		}
 
 		// We don't care about this being cancelled.
 		timeout, _ := cmd.Flags().GetInt(FlagTimeout)
 		nextCtx, _ := context.WithTimeout(cmd.Context(), time.Duration(timeout)*time.Second)
-		cmd.SetContext(ConfigContext(nextCtx, &config, configPath))
+		cmd.SetContext(ConfigContext(nextCtx, &config))
 		return nil
 	},
 	PersistentPostRunE: func(cmd *cobra.Command, _ []string) error {
 		config := ConfigFromCommand(cmd)
-		path := ConfigPathFromCommand(cmd)
-		if expanded, err := homedir.Expand(path); err == nil {
-			path = expanded
+		if err := saveConfig(config); err != nil {
+			return fmt.Errorf("failed to save config: %s", err)
 		}
-
-		// Do not use EnsureConfigFileExists here!
-		// EnsureConfigFileExists opens the file in append mode.
-		// If we open the file in append mode, we'll always append to the file. If we open the file in truncate mode before reading from the file, the content will be truncated _before we read from it_, which will cause a users configuration to be discarded every time we run the program.
-
-		if err := os.MkdirAll(filepath.Dir(path), os.ModeDir|os.FileMode(0755)); err != nil {
-			return err
-		}
-
-		file, err := os.Create(path)
-		if err != nil {
-			return fmt.Errorf("unable to create %s reason: %w", path, err)
-		}
-
-		defer file.Close()
-		return config.Write(file)
+		return nil
 	},
 	SilenceErrors: true,
 	SilenceUsage:  true,
